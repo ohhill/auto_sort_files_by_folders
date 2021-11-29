f3d40c1da6e503ee1d343c681381d6cba5537b80
AlexGillll <sashahylyuk@gmail.com>
2021-11-29 10:37:57 +0200

    editing variables and minor refinementsЄ

--- main.py
+++ main.py
@@ -1,73 +1,61 @@
-# функція відкриває шляш з файлу
-# після чого забирає шлях з яким працюємо
-# або пише введіть шлях який записує у файл
-# а нашо ваще бавитися зі шляхами якщо можна запускати прогу в папці яку треба відсортувати ?
-import re
+# потрібно додати візуал і захист від дибіла
+
+
 import os
 import shutil
 
-def read_file(filename: str) -> str:
-    with open(filename) as file:
-        return file.readline()
 
-def write_file(filename: str, path: str) -> None:
-    with open(filename, 'w') as file:
-        file.write(path)
-
-
-def search_type_file(all_files: list, endswithtext: str) -> list[str]:
+def search_files_by_extension(all_files: list, file_extension: str) -> list[str]:
     result = []
     for file in all_files:
-        if file.endswith(endswithtext):
+        if file.endswith(file_extension):
             result.append(file)
     return result
 
 
-def create_dir(type_file: str, input_path: str) -> str:
-    if not os.path.isdir(f'{input_path}\\{type_file[1:]}'):
-        os.mkdir(f'{input_path}\\{type_file[1:]}')
-    return type_file[1:]
+def create_dir(file_extension: str, input_path: str) -> str:
+    if not os.path.isdir(f'{input_path}\\{file_extension[1:]}'):
+        os.mkdir(f'{input_path}\\{file_extension[1:]}')
+    return file_extension[1:]
 
-# переміщення 1 файла в 1 категорію
-def move_files_category(namesfile: list[str], dir: str, input_path: str) -> None:
-    if os.path.exists(f'{input_path}\\{dir}'):
-        for name in namesfile:
-            if name != 'main.py' and name != 'main.exe' and name != '':
-                shutil.move(f'{input_path}\\{name}', f'{input_path}\\{dir}')
-                print(f'File moved: {name}')
 
-# додати функцію для пошуку форматів файлів ?
-def search_types(input_path: str) -> list[str]:
-    typeses = []
+def move_files_category(list_of_files: list[str], folder: str, input_path: str) -> None:
+    if os.path.exists(f'{input_path}\\{folder}'):
+        for one_file in list_of_files:
+            if one_file != 'main.py' and one_file != 'main.exe' and one_file != '':
+                shutil.move(f'{input_path}\\{one_file}', f'{input_path}\\{folder}')
+                print(f'File moved: {one_file}')
+
+
+def search_file_extensions(input_path: str) -> list[str]:
+    file_extension_list = []
     all_files = os.listdir(input_path)
     for t in all_files:
         filename, file_extension = os.path.splitext(f'{input_path}\\{t}')
         if file_extension != '':
-            typeses.append(file_extension)
-    return typeses
+            file_extension_list.append(file_extension)
+    return file_extension_list
 
 
-def sort_file(type_file: str, input_path: str):
+def sort_file(extension: str, input_path: str):
     all_files = os.listdir(input_path)
+    move_files_category(search_files_by_extension(all_files, extension), create_dir(extension, input_path), input_path)
 
-    move_files_category(search_type_file(all_files, type_file), create_dir(type_file, input_path), input_path)
-
-    # перенести файл в папку txt
-    # о тримати назви файлів які рівні *.txt
-    # файл по черзі перемістити в папку txt
 
 def sort_files(input_path: str):
-    list = search_types(input_path)
-    for type in list:
-        sort_file(type, input_path)
+    list_extensions = search_file_extensions(input_path)
+    for file_extension in list_extensions:
+        sort_file(file_extension, input_path)
+
 
 # що робити з дублями назф файли різні назви однакові ?
 if __name__ == "__main__":
     try:
-        # print(read_file('path.txt'))
-        # write_file('text.txt', 'тут шлях')
-        # print(os.listdir())
-        input_path = input('path: ')
-        sort_files(input_path)
+        input_user_path = input('Please enter the full path to the folder: ')
+        if os.path.exists(input_user_path):
+            sort_files(input_user_path)
+        else:
+            print('Folder not found try again please!')
+
     except Exception as exc:
         print(exc)
--- test.py
+++ test.py
@@ -1,10 +1,8 @@
-import os
-import shutil
-#path = input('path: ')
-path1 = 'D:\Загальне'
-t = os.listdir(path1)
-print(t[1])
-filename, file_extension = os.path.splitext(t[0])
-print(file_extension)
+def search_files_by_extension(all_files: list, file_extension: str) -> list[str]:
+    result = []
+    for file in all_files:
+        if file.endswith(file_extension):
+            result.append(file)
+    return result
 
-shutil.move(f'{path1}\\{filename}{file_extension}', f'{path1}\\manual')
+
